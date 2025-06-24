7cc711ca783d9f1197279654fcb2300a733dd3c2
DIEGOHORVATTI <d.horvattid@gmail.com>
2025-06-24 06:54:51 -0300

    refactor: streamline prisma client exports for better module structure

--- dist/shared/prismaClient.js
+++ dist/shared/prismaClient.js
@@ -2,9 +2,17 @@
 Object.defineProperty(exports, "__esModule", {
     value: true
 });
-Object.defineProperty(exports, "prisma", {
-    enumerable: true,
-    get: function() {
+function _export(target, all) {
+    for(var name in all)Object.defineProperty(target, name, {
+        enumerable: true,
+        get: Object.getOwnPropertyDescriptor(all, name).get
+    });
+}
+_export(exports, {
+    get default () {
+        return _default;
+    },
+    get prisma () {
         return prisma;
     }
 });
@@ -18,3 +26,4 @@ const prisma = globalForPrisma.prisma ?? new _client.PrismaClient({
     }
 });
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+const _default = prisma;
